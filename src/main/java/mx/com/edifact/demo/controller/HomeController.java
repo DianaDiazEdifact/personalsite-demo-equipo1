7ea9023ea36fe6d7c965353a482ea670e2e12cc1
gmelchor-edifact <143840776+gmelchor-edifact@users.noreply.github.com>
2025-04-07 17:41:40 -0600

    Actualizacion controller

--- src/main/java/mx/com/edifact/demo/controller/HomeController.java
+++ src/main/java/mx/com/edifact/demo/controller/HomeController.java
@@ -14,13 +14,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class HomeController {
-    
-    @GetMapping({"/", "/index"})
-    public String index() {
-        return "index";
-    }
-    @GetMapping({"/contact"})
-    public String contact() {
-        return "contact";
-    }
+
+	@GetMapping({ "/", "/index" })
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping({ "/contact" })
+	public String contact() {
+		return "contact";
+	}
+
+	@GetMapping({ "/projects" })
+	public String projects() {
+		return "projects";
+	}
+
+	@GetMapping({ "/resume" })
+	public String resume() {
+		return "resume";
+	}
 }
